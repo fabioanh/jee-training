791f7d2fee2891aae999c86db1fc46ad924f6557
sdoore <oschoot@gmail.com>
2016-06-28 12:05:54 +0200

    Initial jsp with data put directly in html. Post method going to servlet

--- src/main/java/com/adneom/training/BeerServlet.java
+++ src/main/java/com/adneom/training/BeerServlet.java
@@ -30,7 +30,6 @@ public class BeerServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		// response.getWriter().append("Served at:
 		// ").append(request.getContextPath());
 		System.out.println("Hello from GET method.");
@@ -46,7 +45,6 @@ public class BeerServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,27 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+
+	<h1 align='center'>Beer Selection Page</h1>
+
+
+	Select beer characteristics
+	<p>
+	<form action="/beer-app/BeerServlet" method="POST" focus="color">
+
+		Color: <select name="color">
+			<option value="light">light</option>
+			<option value="amber">amber</option>
+			<option value="brown">brown</option>
+			<option value="dark">dark</option>
+			<option value="red">red</option>
+		</select> <br />
+		<br />
+
+		<center>
+			<input type="submit" value="Select Beer Style" />
+		</center>
+
+	</form>
+
 </body>
 </html>
